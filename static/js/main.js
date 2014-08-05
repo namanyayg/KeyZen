4a7213f31aeaf5b1937a01c3ff23abc91d7dcdc4
Namanyay Goel <mail@namanyayg.com>
2014-08-05 22:00:42 +0530

    It now works! Infinite loop + implicit conversions a PITA

--- static/js/main.js
+++ static/js/main.js
@@ -1,67 +1,5 @@
 $(function() {
 
-	var map = {
-		"97": "a",
-		"98": "b",
-		"99": "c",
-		"100": "d",
-		"101": "e",
-		"102": "f",
-		"103": "g",
-		"104": "h",
-		"105": "i",
-		"106": "j",
-		"107": "k",
-		"108": "l",
-		"109": "m",
-		"110": "n",
-		"111": "o",
-		"112": "p",
-		"113": "q",
-		"114": "r",
-		"115": "s",
-		"116": "t",
-		"117": "u",
-		"118": "v",
-		"119": "w",
-		"120": "x",
-		"121": "y",
-		"122": "z",
-		"32": "space-key",
-		"46": "dot"
-	}
-
-	var charMap = {
-		"a": "97",
-		"b": "98",
-		"c": "99", 
-		"d": "100", 
-		"e": "101", 
-		"f": "102", 
-		"g": "103", 
-		"h": "104", 
-		"i": "105", 
-		"j": "106", 
-		"k": "107", 
-		"l": "108", 
-		"m": "109", 
-		"n": "110", 
-		"o": "111", 
-		"p": "112", 
-		"q": "113", 
-		"r": "114", 
-		"s": "115", 
-		"t": "116", 
-		"u": "117", 
-		"v": "118", 
-		"w": "119", 
-		"x": "120", 
-		"y": "121", 
-		"z": "122", 
-		" ": "32",
-		".": "46"
-	}
-
 	var KYZN = function() {
 
 		var timeouts = function() {
@@ -123,6 +61,7 @@ $(function() {
 		startTheGame = function () {
 			var time = 0,
 			$timer = $('.l1 .js-timer');
+
 			$timer.innerHTML = "00";
 
 			$timer.removeClass('instructions').addClass('timer');
@@ -134,19 +73,20 @@ $(function() {
 
 			var textContent = $('.l1 .text-level').html().split(''); // Text as an array
 
-			for (var i = 0, character; character = textContent[i++];) {
+
+			for (var i = 0; i >= textContent.length; i++) {
 				textContent[i] = "<span>" + textContent[i] + "</span>";
 			}
 
 			var inputNumber = 0,
 			correctChars = 0,
 			wrongChars = 0,
-			totalChars = content.length;
+			totalChars = textContent.length;
 
 			$(window).on('keypress', function(e) {
 				var input = e.which.toString();
 
-				if (input === textContent[inputNumber].charCodeAt()) {
+				if (input === textContent[inputNumber].charCodeAt().toString()) {
 					textContent[inputNumber] = "<span class='l-text-correct'>" + textContent[inputNumber] + "</span>";
 					$('.text-level').html(textContent.join(''));
 					correctChars++;
@@ -158,9 +98,14 @@ $(function() {
 
 					wrongChars++;
 				}
+
 				inputNumber++;
 
-				if (inputNumber == content.length ) {
+				if ( input == '8' ) {
+					e.preventDefault();
+				}
+
+				if (inputNumber == textContent.length ) {
 					showStats(wrongChars, correctChars, totalChars, $timer.html());
 					clearInterval(typeTime);
 					$(window).off('keypress');
@@ -177,7 +122,7 @@ $(function() {
 			$(window).on('keypress', function(e) {
 				console.log(e.which);
 				if (e.which == "13") 
-				startTheGame();
+					startTheGame();
 			});
 		},
 
@@ -220,12 +165,6 @@ $(function() {
 
 
 	KYZN().init();
-
-
-	// First level
-	
-
-	// Binds
 
 
 })
